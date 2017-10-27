b49b547b52fc53354229b57470e9d5c96f3d8241
Abel Rangel <ransilad@gmail.com>
2017-10-26 21:17:31 -0300

    Décima clase: Ajax con vue-resource

---
+++ src/App_vue_resource.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+        <img v-for="user in users" :src="user.picture.thumbnail" :alt="user.name.first">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        mounted() {
+            this.cargarPersonas();
+        },
+        methods: {
+            cargarPersonas() {
+                this.$http.get('https://randomuser.me/api/?results=500').then(response => {
+                    console.log(response);
+                    this.users = response.body.results;
+                });
+            }
+        },
+        data() {
+            return {
+                msg: 'Welcome to Vue resource',
+                users: []
+            }
+        }
+    }
+</script>

957c3b3b016a54f41852633826f9a5ad9f35dd79
Abel Rangel <ransilad@gmail.com>
2017-10-27 14:45:24 -0300

    Clase 12: Módulos pasandole parámetros

---
+++ src/App-modularizada.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        <mis-tareas :tareas="tareasAjax"></mis-tareas>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'app',
+        mounted() {
+            this.cargarPersonas();
+        },
+        methods: {
+            cargarPersonas() {
+                axios.get('https://jsonplaceholder.typicode.com/todos').then(response => {
+                    console.log(response);
+                    this.tareasAjax = response.data;
+                });
+            }
+        },
+        data() {
+            return {
+                tareasAjax: []
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
-// import App from './App.vue' // Old
-import App from './App_vue_resource.vue'
+// import App from './App.vue'
+// import App from './App_vue_resource.vue'
+import App from './App-modularizada.vue'
 // import VueResource from 'vue-resource'
 // Vue.use(VueResource);
+
+Vue.component('mis-tareas', {
+    props: ['tareas'],
+    template: `<ul><li v-for="tarea in tareas" v-text="tarea.title"></li></ul>`
+});
 
 new Vue({
     el: '#app',

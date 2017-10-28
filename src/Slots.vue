ac2942ebaa2e107beb29601b25146b31e20e7df9
Abel Rangel <ransilad@gmail.com>
2017-10-28 12:59:27 -0300

    Clase 14: Agregar contenido dinámico con diferentes slots

--- src/Slots.vue
+++ src/Slots.vue
@@ -1,7 +1,20 @@
 <template>
     <div id="app">
         <slots-component>
-            Otro Contenido
+            <template slot="header">Header 1</template>
+            <template>Contenido 1</template>
+            <template slot="footer">Footer 1</template>
+        </slots-component>
+
+        <slots-component>
+            <template slot="header">Header 2</template>
+            <template>Contenido 2</template>
+            <template slot="footer">Footer 2</template>
+        </slots-component>
+
+        <slots-component>
+            <template slot="header">Header 2</template>
+            <template slot="footer">Footer 2</template>
         </slots-component>
     </div>
 </template>

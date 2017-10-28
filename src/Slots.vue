18f571a3441e803d1041a61ab054813ae7660773
Abel Rangel <ransilad@gmail.com>
2017-10-28 12:52:36 -0300

    Clase 14: Agregar contenido agregado desde la instancia y no el por defecto (slots)

---
+++ src/Slots.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+        <slots-component>
+            Otro Contenido
+        </slots-component>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,30 +1,15 @@
 import Vue from 'vue'
 // import App from './App.vue'
 // import App from './App_vue_resource.vue'
+// import App from './App-modularizada.vue'
 // import VueResource from 'vue-resource'
 // Vue.use(VueResource);
-import App from './App-modularizada.vue'
+import App from './Slots.vue'
 
-import axios from 'axios';
+// import axios from 'axios';
 
-Vue.component('mis-tareas', {
-    template: '#plantilla',
-    mounted() {
-        this.cargarPersonas();
-    },
-    methods: {
-        cargarPersonas() {
-            axios.get('https://jsonplaceholder.typicode.com/todos').then(response => {
-                console.log(response);
-                this.tareas = response.data;
-            });
-        }
-    },
-    data() {
-        return {
-            tareas: []
-        }
-    }
+Vue.component('slots-component', {
+    template: '#slots'
 });
 
 new Vue({

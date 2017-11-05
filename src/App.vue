fdb5797591ce9f53a27686a66410a9c204be9a68
Abel Rangel <ransilad@gmail.com>
2017-11-05 18:43:59 -0300

    Use vuex and Vue Router global guards

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,15 @@
         <router-link to="/">Home</router-link>
         <router-link to="/contacto">Contacto</router-link>
         <router-link :to="{name: 'usuario', params: {username: 'ransilad'}}">Usuario</router-link>
+        <button @click="cambiar">Cambiar auth</button>
         <hr>
         <router-view></router-view>
     </section>
 </template>
 
 <script>
+    import {mapMutations} from 'vuex';
     export default {
+        methods: mapMutations(['cambiar'])
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 import {routes} from './routes';
 import App from './App.vue'
+import {store} from './store.js';
 
 Vue.use(VueRouter);
 
@@ -10,8 +11,14 @@ const router = new VueRouter({
     routes
 });
 
+router.beforeEach((to, from, next) => {
+    console.log('Acceso a ruta');
+    next(store.state.auth);
+});
+
 new Vue({
     el: 'main',
     router,
+    store,
     render: h => h(App)
 });

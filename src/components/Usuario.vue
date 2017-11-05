97a1fd610fc5e6f181a7f0b26eabd7c6561219a9
Abel Rangel <ransilad@gmail.com>
2017-11-04 21:08:53 -0300

    Vue Router dynamic routes

---
+++ src/components/Usuario.vue
@@ -0,0 +1,22 @@
+<template>
+    <section>
+        {{ username }}
+    </section>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                username: this.$route.params.username
+            }
+        },
+        watch: {
+            '$route'(to, from) {
+                console.log(to);
+                if (to.params.username !== from.params.username) {
+                    this.username = to.params.username;
+                }
+            }
+        }
+    }
+</script>
